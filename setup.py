365b82f669c93f3d3121e67ff970d598ac24d378
hkang42 <h.kang.q@gmail.com>
2020-04-08 19:00:30 -0400

    added requirements.txt

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-hkang42", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Harrison Kang",
-    author_email="H.Kand.Q@email.com",
+    author_email="H.Kand.Q@gmail.com",
     description="Add new column to DF from list",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
